2e0110ad4894f01a611280aa18ea4e1f54b053b1
ZitaRR <Haeggqvist1998@hotmail.com>
2020-04-23 12:52:43 +0200

    WEB3-113 - Reduced maximum characters allowed to 20

--- MarysMajesticMovies/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MarysMajesticMovies/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -48,13 +48,14 @@ namespace MarysMajesticMovies.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
+            [StringLength(20, MinimumLength = 6)]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
