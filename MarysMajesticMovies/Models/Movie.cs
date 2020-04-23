b64e06331f0f55e67bbf204eaaa34fc8516072e1
AndersOrlinder <anders_orlinder@hotmail.com>
2020-04-24 01:05:32 +0200

    WEB3-31 Added Movie model

---
+++ MarysMajesticMovies/Models/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarysMajesticMovies.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ImdbId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public string RunTime { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string Director { get; set; }
+        [Required]
+        public string Actors { get; set; }
+        [Required]
+        public string Plot { get; set; }
+        [Required]
+        public int ImdbRating { get; set; }
+        [Required]
+        public string PosterUrl { get; set; }
+        [Required]
+        public string TrailerUrl { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int InStock { get; set; }
+    }
+}
