9850560765e062569b0f7682ae26f926c5ec93d0
Karo Massudi <karomassudi@live.se>
2020-04-27 12:55:09 +0200

    Vertical side-bar implemented.
    Vertical side-bar with icon, js-script and addition to style.css

--- MarysMajesticMovies/wwwroot/DesignDrafts/script.js
+++ MarysMajesticMovies/wwwroot/DesignDrafts/script.js
@@ -149,3 +149,15 @@ function randomImg5(){
             document.getElementById("latest").innerHTML += `<img class="movie" src=${src} />`
         }
 }
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("mainpagebody").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("mainpagebody").style.marginLeft = "0";
+}
+
