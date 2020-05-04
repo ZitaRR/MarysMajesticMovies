7a666b49c4f38681c835aaf6c6a8b38ca6f9d33a
Karo Massudi <karomassudi@live.se>
2020-05-04 09:18:08 +0200

    WEB3-22 Minor changes added.
    Icons for menu and fixed margins.

--- MarysMajesticMovies/wwwroot/DesignDrafts/script.js
+++ MarysMajesticMovies/wwwroot/DesignDrafts/script.js
@@ -149,15 +149,22 @@ function randomImg5(){
             document.getElementById("latest").innerHTML += `<img class="movie" src=${src} />`
         }
 }
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("mainpagebody").style.marginLeft = "250px";
 }
 
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("mainpagebody").style.marginLeft = "0";
 }
 
+function openNavRight() {
+    document.getElementById("mySidebarRight").style.width = "250px";
+    document.getElementById("mainpagebody").style.marginRight = "250px";
+}
+
+function closeNavRight() {
+    document.getElementById("mySidebarRight").style.width = "0";
+    document.getElementById("mainpagebody").style.marginRight = "0";
+}
